ec0457451265338f8f1f503a2cf951c182db80c4
Jesna <jesnafathima1997@gmail.com>
2025-04-06 18:36:45 +0530

    router.jsx name changed to Router.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { RouterProvider } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
-import  router  from "./routes/Router";
+import  Router  from "./routes/Router";
 
 function App() {
     return (
         <>
-            <RouterProvider router={router} />
+            <RouterProvider Router={Router} />
             <Toaster />
         </>
     );
